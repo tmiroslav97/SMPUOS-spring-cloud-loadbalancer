ab206d6fa1c70dbdc4a9aa19eb75c054534f402d
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-13 15:59:21 +0100

    Renaming zuul application

---
+++ Zuul/src/test/java/rs/ac/uns/ftn/acs/Zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.acs.Zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
