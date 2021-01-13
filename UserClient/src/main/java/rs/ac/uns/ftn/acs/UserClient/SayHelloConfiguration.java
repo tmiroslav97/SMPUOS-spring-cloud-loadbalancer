a1c3227b4e9166f39ed0b758498c642d92b4ddcd
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-13 13:12:35 +0100

    Modified spring cloud loadbalancer configuration to work automatically with service registry

--- UserClient/src/main/java/rs/ac/uns/ftn/acs/UserClient/SayHelloConfiguration.java
+++ UserClient/src/main/java/rs/ac/uns/ftn/acs/UserClient/SayHelloConfiguration.java
@@ -13,32 +13,32 @@ import java.util.List;
 
 @Configuration
 public class SayHelloConfiguration {
-
-    @Bean
-    @Primary
-    ServiceInstanceListSupplier serverInstancesListSupplier() {
-        return new DemoServiceInstanceListSuppler("say-hello");
-    }
+//
+//    @Bean
+//    @Primary
+//    ServiceInstanceListSupplier serverInstancesListSupplier() {
+//        return new DemoServiceInstanceListSuppler("say-hello");
+//    }
 }
 
-class DemoServiceInstanceListSuppler implements ServiceInstanceListSupplier {
-
-    private final String serviceId;
-
-    DemoServiceInstanceListSuppler(String serviceId) {
-        this.serviceId = serviceId;
-    }
-
-    @Override
-    public String getServiceId() {
-        return serviceId;
-    }
-
-    @Override
-    public Flux<List<ServiceInstance>> get() {
-        return Flux.just(Arrays
-                .asList(new DefaultServiceInstance(serviceId + "1", serviceId, "localhost", 8090, false),
-                        new DefaultServiceInstance(serviceId + "2", serviceId, "localhost", 8091, false),
-                        new DefaultServiceInstance(serviceId + "3", serviceId, "localhost", 8092, false)));
-    }
-}
+//class DemoServiceInstanceListSuppler implements ServiceInstanceListSupplier {
+//
+//    private final String serviceId;
+//
+//    DemoServiceInstanceListSuppler(String serviceId) {
+//        this.serviceId = serviceId;
+//    }
+//
+//    @Override
+//    public String getServiceId() {
+//        return serviceId;
+//    }
+//
+//    @Override
+//    public Flux<List<ServiceInstance>> get() {
+//        return Flux.just(Arrays
+//                .asList(new DefaultServiceInstance(serviceId + "1", serviceId, "localhost", 8090, false),
+//                        new DefaultServiceInstance(serviceId + "2", serviceId, "localhost", 8091, false),
+//                        new DefaultServiceInstance(serviceId + "3", serviceId, "localhost", 8092, false)));
+//    }
+//}
