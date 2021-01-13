e5f43b2e8869b91e1f57f85510b84266a45f33ee
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-13 13:12:49 +0100

    Adding zuul gateway

---
+++ Zull/src/test/java/rs/ac/uns/ftn/acs/Zull/ZullApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.acs.Zull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
